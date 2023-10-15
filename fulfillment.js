f0e7e3f52ec8f6c2d0849653960e3f6edef3b50e
vfaillace <52092997+vfaillace@users.noreply.github.com>
2023-10-14 23:23:52 -0400

    trade-in intent, upgrade IP

--- fulfillment.js
+++ fulfillment.js
@@ -35,14 +35,11 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         data: "",
       })
         .then((response) => {
-          var json = response.data.iphone_prices;
-          var phone = smartphone.data;
-          var price = json[phone];
-          if (price) {
-            agent.add(`The price of ${smartphone} is ${price}`);
-          } else {
-            agent.add(`The price for ${smartphone} is not available. Please check back later.`);
-          }
+          const json = response.data.iphone_prices;
+          const phone = smartphone;
+          const price = json[phone];
+          const upgradeInfo = getUpgradeInfo(phone);
+          agent.add(`The price of ${phone} is ${price}. Here is the plan upgrade information: ${upgradeInfo}`);
         })
         .catch((error) => {
           console.error('Error fetching data:', error);
@@ -51,87 +48,58 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     }
   }
 
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  intentMap.set('Trade-in', Tradein);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
-  agent.handleRequest(intentMap);
-});
-// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
-// for Dialogflow fulfillment library docs, samples, and to report issues
-'use strict';
- 
-const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
-
-//Added for tutorial
-const axios = require('axios');
- 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
- 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
- 
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
-  }
- 
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
+  function getUpgradeInfo(phone) {
+    const betterPromotionPhones = ["iPhone 8", "iPhone 9", "iPhone 10", "iPhone 11", "iPhone 12",
+      "iPhone 13", "iPhone 14", "iPhone 15"];
+    if (betterPromotionPhones.includes(phone)) {
+      return "For the eligible phones, the trade-in values are $1000 for the Unlimited Ultimate plan, $830 for the Unlimited Plus plan, and $415 for the Unlimited Welcome plan.";
+    } else {
+      return "For other phones, the maximum trade-in value is $830 for the Unlimited Ultimate plan, $415 for the Unlimited Plus plan, and $100 for the Unlimited Welcome plan.";
+    }
   }
 
-  function Tradein(agent){
-    //Here we get the type of the utterance
-    const smartphone = agent.parameters.smartphone;
 
-    if (smartphone) {
+  function Upgrade(agent) {
+    const smartphones = agent.parameters.smartphone;
+
+    if (smartphones && smartphones.length >= 2) {
+      const phone1 = smartphones[0]; // Current phone
+      const phone2 = smartphones[1]; // New phone
+      agent.add(phone1 + phone2)
+      console.log(phone1 + phone2)
       return axios({
         method: "GET",
         url: "https://raw.githubusercontent.com/dannirash/UF_Hackathon/iphone_prices.json",
         data: "",
       })
         .then((response) => {
-          var json = response.data.iphone_prices;
-          var phone = smartphone;
-          var price = json[phone];
-          agent.add(`The price of ${phone} is ${price}`);
+          const json = response.data.iphone_prices;
+          const price2 = getPrice(json, phone2);
+
+          agent.add(`Trading in your ${phone1} for an ${phone2} would be:`);
+          calculatePromotion("Unlimited Ultimate plan", 1000, 90, price2);
+          calculatePromotion("Unlimited Plus plan", 850, 80, price2);
+          calculatePromotion("Unlimited Welcome plan", 415, 65, price2);
+
         })
         .catch((error) => {
-          console.log(error);
+          console.error('Error fetching data:', error);
+          agent.add("There was an error fetching pricing information. Please try again later.");
         });
+    } else {
+      agent.add("Please provide at least two valid phones for comparison.");
     }
   }
 
-  function Upgrade(agent){
-    //Here we get the type of the utterance
-    const smartphone = agent.parameters.smartphone;
-
-    if (smartphone) {
-      return axios({
-        method: "GET",
-        url: "https://raw.githubusercontent.com/dannirash/UF_Hackathon/iphone_prices.json",
-        data: "",
-      })
-        .then((response) => {
-          var json = response.data.iphone_prices;
-          var phone = smartphone;
-          var price1 = json[phone[0]];
-          var price2 = json[phone[1]];
-          var result = price2 - price1;
-          agent.add(`The price diffrence is ${result}`);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
+  function getPrice(json, phone) {
+    return json[phone] || null;
   }
+
+  function calculatePromotion(plan, tradeInValue, monthlyPrice, price) {
+    const result = tradeInValue - price + monthlyPrice;
+    agent.add(`- ${result}$ under ${plan}`);
+  }
+
 
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
